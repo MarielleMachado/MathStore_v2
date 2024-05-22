d514e9e1695df4bb7a73e3d0a9194031a7b99258
MarielleMachado <162848331+MarielleMachado@users.noreply.github.com>
2024-05-21 21:03:52 -0600

    creación de la tabla users

---
+++ back-end/src/main/java/com/BackEnd/MathStoreOnline2/entities/User.java
@@ -0,0 +1,19 @@
+package com.BackEnd.MathStoreOnline2.entities;
+
+import com.BackEnd.MathStoreOnline2.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private UserRole userRole;
+    private  byte[] img;
+}
