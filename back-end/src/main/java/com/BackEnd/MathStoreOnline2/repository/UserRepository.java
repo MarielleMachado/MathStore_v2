3affc17daa2246be2ddc70d9f8dbe352af2bb565
MarielleMachado <162848331+MarielleMachado@users.noreply.github.com>
2024-05-21 21:06:20 -0600

    Creación del repositorio user

---
+++ back-end/src/main/java/com/BackEnd/MathStoreOnline2/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.BackEnd.MathStoreOnline2.repository;
+
+import com.BackEnd.MathStoreOnline2.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
