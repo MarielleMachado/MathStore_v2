bc312d3647a33ddc8b3050373cf59d495063d67c
MarielleMachado <162848331+MarielleMachado@users.noreply.github.com>
2024-05-21 20:38:11 -0600

    Proyecto Base

---
+++ back-end/src/main/java/com/BackEnd/MathStoreOnline2/MathStoreOnline2Application.java
@@ -0,0 +1,13 @@
+package com.BackEnd.MathStoreOnline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathStoreOnline2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathStoreOnline2Application.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/BackEnd/MathStoreOnline2/MathStoreOnline2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackEnd.MathStoreOnline2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathStoreOnline2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
